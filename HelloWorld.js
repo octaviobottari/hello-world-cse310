c19622d2f50d26092e0c32fb387d816ad2711d93
octaviobottari <octaviobottari7@gmail.com>
2026-05-08 12:09:01 -0300

    HelloWorld feature

---
+++ HelloWorld.js
@@ -0,0 +1,10 @@
+// Hello World program for CSE 310
+// This program prints a greeting message to the console
+
+function sayHello() {
+    console.log("Hello World!");
+    console.log("Welcome to my CSE 310 portfolio!");
+}
+
+// Call the function
+sayHello();
